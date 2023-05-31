4117c41aea26f5b9ab7f11911991ebbbf15e35bb
isidoraoOo <135176574+isidoraoOo@users.noreply.github.com>
2023-05-31 18:04:34 -0400

    Update funciones_finales.cpp

--- EDD2/TAREAFINAL/funciones_finales.cpp
+++ EDD2/TAREAFINAL/funciones_finales.cpp
@@ -15,7 +15,11 @@ Arbol::Arbol(){
 }
 
 //*********************************
-//funciones auxiliares
+
+//Funciones auxiliares
+//A continuación se encuentran las funciones auxiliares, las que 
+//posteriormente se utilizarán para las funciones solicitadas
+
 Nodo* Arbol::crearNodo(tElem pos){
 
     Nodo* nuevoNodo = new Nodo();
@@ -24,10 +28,14 @@ Nodo* Arbol::crearNodo(tElem pos){
     nuevoNodo->derecho = NULL;
 
     return nuevoNodo;
-
-
 }
-//funcion auxiliar de insert
+
+//Funcion auxiliar de insert
+//Su proposito es insetar un nodo en el arbol. Si el arbol esta vacio, crea un 
+//nuevo nodo y lo asigna raiz. Si no esta vacio, compara el elemento que se desea 
+//insertar con la raiz actual con el proposito de determinar en que subarbol debe 
+//ser insertado el nuevo elemento.
+
 void Arbol::insertarNodo(tElem pos, Nodo* &arbol){
 
     if(arbol == NULL){
@@ -51,7 +59,15 @@ void Arbol::insertarNodo(tElem pos, Nodo* &arbol){
         }
     }
 }
-//funcion auxiliar de find
+
+//Funcion auxiliar de find
+//Su proposito es buscar un determinado elemento en el arbol. Si el arbol no existe
+//retornara 'false', pues no hay elementos en el arbol. Si el arbol existe
+//ira comparando el elemento que se esta buscando con la raiz del arbol. La busqueda 
+//contunuara con llamadas recursivas en el subarbol derecho o izquierdo, dependiendo 
+//de la relacion entre el elemento buscado y la raiz. Si se encuentra el elemento se 
+//retornara 'true', de otra manera retornara 'false'.
+
 bool Arbol::busqueda(tElem pos, Nodo* arbol){
 
     if(arbol == NULL){
@@ -79,7 +95,13 @@ bool Arbol::busqueda(tElem pos, Nodo* arbol){
     }
 }
 
-//funcion auxiliar de lower_bound
+//Funcion auxiliar de lower_bound
+//Su proposito es buscar el primer elemento que sea menor o igual a otro elemento 'pos'.
+//Compara la informacion del nodo actual con el elemento 'pos' para determinar por que 
+//subarbol se debe continuar la busqueda. La exploracion en los subarboles correspondientes
+//se realiza con llamadas recursivas. Finalmente, retornara el numero mas cercano
+//que es menor o igual a 'pos' si es que es encontrado, de otra forma retornara -1.
+
 int Arbol::encontrarNodo(tElem pos,Nodo *arbol) {
     if (arbol == NULL) {
         return -1;
@@ -96,7 +118,8 @@ int Arbol::encontrarNodo(tElem pos,Nodo *arbol) {
     return num;
     }
 }
-//funcion auxiliar de clear
+//Funcion auxiliar de clear
+//Su proposito es eliminar todos los nodos del arbol de manera recursiva
 
 void Arbol::clearHelp(Nodo* arbol) {
     if (arbol == NULL) return;
@@ -106,22 +129,26 @@ void Arbol::clearHelp(Nodo* arbol) {
     delete arbol;
 }
 
-//funciones
+//Funciones
+//Estas funciones son las que se solicitaron en el enunciado. El proposito de crear 
+//funciones auxiliares fue facilitar su desarrollo y luego crear estas funciones 
+//principales, que llamarian a las auxiliares. De esta manera, las funciones principales 
+//solo tendrian el parametro requerido.
 
-//funcion insert
+//Funcion insert
 void Arbol::insert(tElem pos){
 
     insertarNodo(pos, arbol);
 
 }
-//funcion find
+//Funcion find
 bool Arbol::find(tElem pos){
 
     return busqueda(pos, arbol);
 
 }
 
-//funcion lower_bound
+//Funcion lower_bound
 
 int Arbol::lower_bound(tElem pos){
 
@@ -129,7 +156,7 @@ int Arbol::lower_bound(tElem pos){
 
 }
 
-//funcion clear
+//Funcion clear
 
 void Arbol::clear(){
 
